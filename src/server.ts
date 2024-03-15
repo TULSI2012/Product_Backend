6b52fbb9253cfbc2f4f8b6c43e5377cc25d7d2f8
Tulsi kalola <66298634+TULSI2012@users.noreply.github.com>
2024-03-15 17:31:03 -0400

    add sever file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,32 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 const app = express();
 
-const PORT: string | number = process.env.PORT ?? 8000;
+const PORT = 8000;
 
-const server = app.listen(PORT, () => {
+// MIDDLEWARE
+app.use(express.json({ limit: "10kb" }));
+
+const todos = [
+  { id: 1, title: "coding" },
+  { id: 2, title: "cleaning" },
+];
+
+app.get("/welcomemsg", (req, res) => {
+  res.status(200).send("Welcome Tulsi");
+});
+
+// POST Request which console of User added
+app.post("/welcomemsg", (req, res) => {
+  const { username } = req.body;
+  res.status(201).send(`User Created ${username} `);
+});
+
+app.delete("/welcomemsg/:id", (req, res) => {
+  const { id } = req.params;
+  // remove it from the array
+  res.status(204).send(`User Deleted Successfully`);
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
