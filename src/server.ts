58c457e70908235a3ae727342afd27c6123fc4ff
Tulsi kalola <66298634+TULSI2012@users.noreply.github.com>
2024-03-15 10:39:55 -0400

    server file update

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Server is Running")
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+
+const PORT: string | number = process.env.PORT ?? 8000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
