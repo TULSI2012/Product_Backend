7fcfd42c0d3d1e597b17e1ad02dfa02e338a2828
Tulsi kalola <66298634+TULSI2012@users.noreply.github.com>
2024-03-19 09:22:39 -0400

    remove comments

--- src/Routes/productRouters.ts
+++ src/Routes/productRouters.ts
@@ -7,7 +7,6 @@ const router = express.Router();
 // localhost:8000/todos/ GET, POST
 router.route("/").get(getAllProduct).post(addProcuct);
 
-// localhost:8000/todos/:id
-// router.route("/:id").get(getAllTodos);
+
 
 export default router;
